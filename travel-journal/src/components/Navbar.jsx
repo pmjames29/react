8ecc2973aa20447073ca36a0782ebeb2d7f4e19d
pmjames29 <pmjames29@gmail.com>
2022-08-24 15:53:18 -0400

    Added travel journal react project, as well as renamed portfolio card project's folder name.

---
+++ travel-journal/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className='nav'>
+            <img src='../../public/earthlogo.png' alt='earth_logo' />
+            <h2 className='nav--title'>my travel journal.</h2>
+        </nav>
+    )
+}
---
+++ travel-journal/src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        title: 'Old Baldy Lighthouse',
+        location: 'NORTH CAROLINA',
+        googleMapsUrl: 'https://goo.gl/maps/Ujn98nXbhw6kXRH49',
+        startDate: '12 Aug, 2022',
+        endDate: '19 Aug, 2022',
+        description: 'Old Baldy is the oldest standing lighthouse in North Carolina. It has maintained its original form and location for over 187 years. The most important purpose of Old Baldy was to mark the Cape Fear River entrance for the maritime sailors.',
+        imageURL: 'oldbaldy.png'
+    },
+    {
+        id: 2,
+        title: 'La Sagrada Familia',
+        location: 'SPAIN',
+        googleMapsUrl: 'https://goo.gl/maps/ASKd1AyEn6pEqte49',
+        startDate: '11 Mar, 2019',
+        endDate: '15 Mar, 2019',
+        description: 'The Basilica i Temple Expiatori de la Sagrada Familia is a large unfinished church building in the Eixample district of Barcelona, Catalonia, Spain, and is currently the largest unfinished Roman Catholic church. Designed by the Catalan architect Antoni Gaudi, his work on Sagrada Familia is part of a UNESCO World Heritage Site.',
+        imageURL: 'sagradafamilia.png'
+    },
+    {
+        id: 3,
+        title: 'Eiffel Tower',
+        location: 'FRANCE',
+        googleMapsUrl: 'https://goo.gl/maps/VdVjrXwsAze2Y3zg7',
+        startDate: '18 Jun, 2017',
+        endDate: '21 Jun, 2017',
+        description: 'The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.',
+        imageURL: 'eiffel.png'
+    }
+]
